a59ce223367edeffb7d1e45852fa76ff5f9a4e74
cryptapus <info@cryptap.us>
2018-10-31 17:22:27 -0500

    name changes in license

--- index.php
+++ index.php
@@ -6,18 +6,18 @@ Copyright 2018 cryptapus <info@cryptapus.org>
  
 This file is part of php-coinbase-miners.
  
-JSON-RPC PHP is free software; you can redistribute it and/or modify
+php-coinbase-miners is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 2 of the License, or
 (at your option) any later version.
  
-JSON-RPC PHP is distributed in the hope that it will be useful,
+php-coinbase-miners is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
  
 You should have received a copy of the GNU General Public License
-along with JSON-RPC PHP; if not, write to the Free Software
+along with php-coinbase-miners; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
