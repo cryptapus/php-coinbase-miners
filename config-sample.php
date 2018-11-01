9d3b0566b81a16661d4e73a182a6a826e63283e6
cryptapus <info@cryptap.us>
2018-11-01 17:16:03 -0500

    relicensed to MIT, jsonRPCClient -> easybitcoin

--- config-sample.php
+++ config-sample.php
@@ -1,7 +1,8 @@
 <?php
 
 return [
-    'rpcaddress' => '127.0.0.1:10889',
+    'rpchost' => '127.0.0.1',
+    'rpcport' => 10889,
     'rpcusername' => 'someusername',
     'rpcpassword' => 'somepassword',
     'blocksperpage' => 50,
--- index.php
+++ index.php
@@ -1,24 +1,30 @@
 <?php
 /*
-					COPYRIGHT
- 
-Copyright 2018 cryptapus <info@cryptapus.org>
- 
-This file is part of php-coinbase-miners.
- 
-php-coinbase-miners is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
- 
-php-coinbase-miners is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
- 
-You should have received a copy of the GNU General Public License
-along with php-coinbase-miners; if not, write to the Free Software
-Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+====================
+
+The MIT License (MIT)
+
+Copyright (c) 2018 cryptapus <info@cryptapus.org>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+====================
 */
 
 $config = include('config.php');
@@ -28,12 +34,13 @@ $settings = [
     'latest_version' => '00',
     'explorer_link_blk' => 'https://blockbook.myralicious.com/block/',
     'release_link' => 'https://www.reddit.com/r/myriadcoin/comments/9l0apt/myriadcoin_01440/',
+    'rpchost' => $config['rpchost'],
+    'rpcport' => $config['rpcport'],
     'rpcusername' => $config['rpcusername'],
     'rpcpassword' => $config['rpcpassword'],
-    'rpcaddress' => $config['rpcaddress'],
 ];
 
-require_once('jsonRPCClient.php');
+require_once('easybitcoin.php');
 
 date_default_timezone_set('UTC');
 
@@ -63,8 +70,8 @@ function getminerinfo($cb) {
 }
 
 function blocklist($settings) {
-    $coin = new jsonRPCClient("http://".$settings['rpcusername'].":".
-        $settings['rpcpassword']."@".$settings['rpcaddress']);
+    $coin = new Bitcoin($settings['rpcusername'],$settings['rpcpassword'],
+        $settings['rpchost'],$settings['rpcport']);
     $info = $coin->getinfo();
     $tipblock = $info["blocks"];
     if (array_key_exists('startblock',$_GET)) {
